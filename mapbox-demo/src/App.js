0a4838c28509a3ac3b93ad1854399cb25dd73459
Brian Illes <brianrilles@gmail.com>
2019-04-22 20:48:47 -0700

    Adds custom map

--- mapbox-demo/src/App.js
+++ mapbox-demo/src/App.js
@@ -1,18 +1,28 @@
-import React, {useState} from 'react';
-import ReactMapGL from "react-map-gl"
+import React, { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
 import './App.css';
 
 export default function App() {
   const [viewport, setViewport] = useState({
-    width: "800px",
-    height: "80vh",
+    width: '1000px',
+    height: '95vh',
     latitude: 37.7749,
     longitude: -122.4194,
     zoom: 8
-  })
+  });
 
-
-  return(
-    <div className="map"><ReactMapGL {...viewport} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}>DEMO MAP</ReactMapGL></div>
-  )
+  return (
+    <div className="map">
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/brilles/cjut8h6nagdt61fo283ra2oof"
+        onViewportChange={viewport => {
+          setViewport(viewport);
+        }}
+      >
+        DEMO MAP
+      </ReactMapGL>
+    </div>
+  );
 }
